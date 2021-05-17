24e4a8accf78fc418e84343c0e70c780517af0c9
mk <collobert@juniorinsaservices.fr>
2021-05-17 11:48:09 +0200

    add: monitor et backup script

--- examples/wav2vec/backup.sh
+++ examples/wav2vec/backup.sh
@@ -7,7 +7,7 @@ do
     python libs/fairseq/examples/wav2vec/training_monitor.py --log results/hydra_train.log --output results/
     sleep 2
     echo Zip graphs
-    zip graphs results/*.png
+    zip results/graphs results/*.png
     echo Upload to S3
     aws s3 cp results/hydra_train.log s3://dl4s-datasets/0$1-backup/
     aws s3 cp results/graphs.zip s3://dl4s-datasets/0$1-backup/
