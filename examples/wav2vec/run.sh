003d9408cf09e64333e5aa3900aa205e3afb0a2e
mk <collobert@juniorinsaservices.fr>
2021-05-17 12:14:38 +0200

    add sleep

--- examples/wav2vec/run.sh
+++ examples/wav2vec/run.sh
@@ -3,6 +3,7 @@ cp -v `pwd`/data/WP1_$3/train.tsv `pwd`/data/WP1_$1/dev_other.tsv
 cp -v `pwd`/data/WP1_$3/train.ltr `pwd`/data/WP1_$1/dev_other.ltr
 cp -v `pwd`/data/WP1_$3/train.wrd `pwd`/data/WP1_$1/dev_other.wrd
 echo Running training on `pwd`/data/WP1_$1 with base_$2 config
+sleep 4
 fairseq-hydra-train \
     distributed_training.distributed_world_size=1 \
     task.data=`pwd`/data/WP1_$1 \
