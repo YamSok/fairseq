a9ade6ff5dda318c2d18bed7acbb3f4e0b1ffc40
mk <collobert@juniorinsaservices.fr>
2021-05-17 12:03:24 +0200

    backup

--- examples/wav2vec/backup.sh
+++ examples/wav2vec/backup.sh
@@ -5,10 +5,10 @@ do
     sleep $2
     echo Generate graphs
     python libs/fairseq/examples/wav2vec/training_monitor.py --log results/hydra_train.log --output results/
-    sleep 2
     echo Zip graphs
     zip results/graphs results/*.png
     echo Upload to S3
+    aws s3 cp results/checkpoint_last.pt s3://dl4s-datasets/0$1-backup/
     aws s3 cp results/hydra_train.log s3://dl4s-datasets/0$1-backup/
     aws s3 cp results/graphs.zip s3://dl4s-datasets/0$1-backup/
 done
