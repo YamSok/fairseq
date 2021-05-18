60c2110ec915515911c49eb51693df7912128c72
mk <collobert@juniorinsaservices.fr>
2021-05-18 11:25:49 +0200

    fzfez

--- examples/wav2vec/backup.py
+++ examples/wav2vec/backup.py
@@ -8,13 +8,15 @@ parser.add_argument("--dest", default=None, type=str,
 parser.add_argument("--sleep", default=None, type=str,
                     required=True, help="time (s) between each backup")
 args = parser.parse_args()
+dest = args.dest
+sleep_time = args.sleep
 
 run('touch results/placeholder.png', shell=True)
 
 while True:
     run("python libs/fairseq/examples/wav2vec/training_monitor.py --log results/hydra_train.log --output results/", shell=True)
     run("zip results/graphs results/*.png", shell=True)
-    run(f"aws s3 cp results/checkpoint_last.pt s3://dl4s-datasets/0{args.dest}-backup", shell=True)
-    run(f"aws s3 cp results/hydra_train.log s3://dl4s-datasets/0{args.dest}-backup/", shell=True)
-    run(f"aws s3 cp results/graphs.zip s3://dl4s-datasets/0{args.dest}-backup", shell=True)
-    time.sleep(args.sleep)
+    run(f"aws s3 cp results/checkpoint_last.pt s3://dl4s-datasets/0{dest}-backup", shell=True)
+    run(f"aws s3 cp results/hydra_train.log s3://dl4s-datasets/0{dest}-backup/", shell=True)
+    run(f"aws s3 cp results/graphs.zip s3://dl4s-datasets/0{dest}-backup", shell=True)
+    time.sleep(sleep_time)
