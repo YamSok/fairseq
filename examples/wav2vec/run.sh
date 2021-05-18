e0c48131b0f7c7f4c61a8ff9619a9e8e62604981
mk <collobert@juniorinsaservices.fr>
2021-05-18 10:41:59 +0200

    run

--- examples/wav2vec/run.sh
+++ examples/wav2vec/run.sh
@@ -3,6 +3,7 @@ cp -v `pwd`/data/WP1_$3/train.tsv `pwd`/data/WP1_$1/dev_other.tsv
 cp -v `pwd`/data/WP1_$3/train.ltr `pwd`/data/WP1_$1/dev_other.ltr
 cp -v `pwd`/data/WP1_$3/train.wrd `pwd`/data/WP1_$1/dev_other.wrd
 echo Running training on `pwd`/data/WP1_$1 with base_$2 config
+source activate p3
 sleep 4
 fairseq-hydra-train \
     distributed_training.distributed_world_size=1 \
@@ -11,4 +12,5 @@ fairseq-hydra-train \
     checkpoint.save_dir=`pwd`/results/ \
     hydra.run.dir=`pwd`/results/ \
     --config-dir `pwd`/libs/fairseq/examples/wav2vec/config/finetuning \
-    --config-name base_$2 
+    --config-name base_$2 &
+disown
