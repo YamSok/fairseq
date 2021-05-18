5e7f482796659a456858dce833c9ab28872f4c3a
mk <collobert@juniorinsaservices.fr>
2021-05-18 11:00:03 +0200

    scripts

--- examples/wav2vec/backup.sh
+++ examples/wav2vec/backup.sh
@@ -2,7 +2,6 @@ source activate p3
 touch results/placeholder.png
 while true;
 do
-    sleep $2
     echo Generate graphs
     python libs/fairseq/examples/wav2vec/training_monitor.py --log results/hydra_train.log --output results/
     echo Zip graphs
@@ -11,4 +10,6 @@ do
     aws s3 cp results/checkpoint_last.pt s3://dl4s-datasets/0$1-backup/
     aws s3 cp results/hydra_train.log s3://dl4s-datasets/0$1-backup/
     aws s3 cp results/graphs.zip s3://dl4s-datasets/0$1-backup/
+    echo 
+    sleep $2
 done
--- examples/wav2vec/run.sh
+++ examples/wav2vec/run.sh
@@ -5,7 +5,7 @@ cp -v `pwd`/data/WP1_$3/train.wrd `pwd`/data/WP1_$1/dev_other.wrd
 echo Running training on `pwd`/data/WP1_$1 with base_$2 config
 source activate p3
 sleep 4
-fairseq-hydra-train \
+nohup fairseq-hydra-train \
     distributed_training.distributed_world_size=1 \
     task.data=`pwd`/data/WP1_$1 \
     model.w2v_path=`pwd`/model/wav2vec_small.pt \
@@ -13,4 +13,3 @@ fairseq-hydra-train \
     hydra.run.dir=`pwd`/results/ \
     --config-dir `pwd`/libs/fairseq/examples/wav2vec/config/finetuning \
     --config-name base_$2 &
-disown
