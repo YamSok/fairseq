4413398eb7a9163d5f0670f579751e2c82887c2a
mk <collobert@juniorinsaservices.fr>
2021-05-19 16:02:26 +0200

    correct bug import

--- examples/wav2vec/hg_training.py
+++ examples/wav2vec/hg_training.py
@@ -163,7 +163,6 @@ def data_preparation():
             pad_token="[PAD]", word_delimiter_token="|")
         feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, \
             sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=True)
-        global processor
         processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
         processor.save_pretrained('results_hg/processor/')
 
