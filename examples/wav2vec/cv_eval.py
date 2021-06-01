ca143e89dca0a52e2dc659c56f4099e7647e70d8
mk <collobert@juniorinsaservices.fr>
2021-06-01 15:05:54 +0200

    allo

--- examples/wav2vec/cv_eval.py
+++ examples/wav2vec/cv_eval.py
@@ -1,8 +1,10 @@
+import ctcdecode
 import torchaudio
 from datasets import load_dataset, load_metric
 from transformers import (
     Wav2Vec2ForCTC,
     Wav2Vec2Processor,
+    Wav2Vec2CTCTokenizer,
 )
 import time
 import torch
@@ -12,6 +14,7 @@ import argparse
 import random
 import pandas as pd
 import os
+import numpy as np
 
 
 def map_to_array(batch):
@@ -39,10 +42,8 @@ def map_to_pred(batch):
     pred_ids = torch.argmax(logits, dim=-1)
     batch["predicted"] = processor.batch_decode(pred_ids)
     batch["target"] = batch["sentence"]
+    batch["corrected"] = decoder.decode_batch(logits.numpy())
     # batch["target"] = batch["text"]
-
-
-    # add lm
     return batch
 
 def show_random_elements(dataset, out, num_examples=10):
@@ -74,16 +75,19 @@ def main():
     results = result
     wer_metric = load_metric("wer")
     wer = wer_metric.compute(predictions=results["predicted"], references=results["target"])
+    wer_corrected = wer_metric.compute(predictions=results["corrected"], references=results["target"])
     print(f"Dataset : Common-voice FR train split" + 
         f"\nInference time : {duration:.2f} \n" + 
-        "Test WER: {:.3f}".format(wer))
+        "Test WER: {:.3f}".format(wer) +
+        "Test WER corrected: {:.3f}".format(wer_corrected))
     print("\n")
     show_random_elements(results, out, num_examples=10)
     wer_log = os.path.join(out, "wer_cv.txt")
     with open(wer_log, "w") as err_file:
         print(f"Dataset : Common-voice FR train split" + 
         f"\nInference time : {duration:.2f} \n" + 
-        "Test WER: {:.3f}".format(wer), file=err_file)
+        "Test WER: {:.3f}".format(wer), file=err_file +
+        "Test WER corrected: {:.3f}".format(wer_corrected))
 
 
 parser = argparse.ArgumentParser()
@@ -93,17 +97,41 @@ parser.add_argument("--processor", default=None, type=str,
                     required=False, help="Valid ata subset label")
 parser.add_argument("--model", default=None, type=str,
                     required=False, help="Model folder")
-                 
+parser.add_argument("--lm", default=None, type=str,
+                    required=False, help="Path to kenlm lm")
 args = parser.parse_args()
 processor_dir = args.processor
 model_dir = args.model
 out = args.out
+lm = args.lm
 
 device = "cuda"
 chars_to_ignore_regex = '[\,\?\.\!\-\;\:\"]'  # noqa: W605
 resampler = torchaudio.transforms.Resample(orig_freq=48_000, new_freq=16_000)
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53-french")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53-french").to(device)
+tokenizer = Wav2Vec2CTCTokenizer.from_pretrained("facebook/wav2vec2-large-xlsr-53-french")
+
+
+vocab_dict = tokenizer.get_vocab()
+sort_vocab = sorted((value, key) for (key,value) in vocab_dict.items())
+vocab = [x[1].replace("|", " ") if x[1] not in tokenizer.all_special_tokens else "_" for x in sort_vocab]
+vocab = [x.lower() for x in vocab]
+
+vocabulary = vocab
+alpha = 2.5 # LM Weight
+beta = 0.0 # LM Usage Reward
+word_lm_scorer = ctcdecode.WordKenLMScorer(lm, alpha, beta) # use your own kenlm model
+decoder = ctcdecode.BeamSearchDecoder(
+    vocabulary,
+    num_workers=2,
+    beam_width=128,
+    scorers=[word_lm_scorer],
+    cutoff_prob=np.log(0.000001),
+    cutoff_top_n=40
+)
+
+
 # processor = Wav2Vec2Processor.from_pretrained(processor_dir)
 # model = Wav2Vec2ForCTC.from_pretrained(model_dir).to("cuda")
 
