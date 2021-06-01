ee5184218b66e8889a6d813015650757b74e892d
mk <collobert@juniorinsaservices.fr>
2021-06-01 14:46:12 +0200

    allo

--- examples/wav2vec/cv_eval.py
+++ examples/wav2vec/cv_eval.py
@@ -12,23 +12,37 @@ import argparse
 import random
 import pandas as pd
 import os
+
+
+# def map_to_array(batch):
+#     speech, _ = torchaudio.load(batch["path"])
+#     batch["speech"] = resampler.forward(speech.squeeze(0)).numpy()
+#     batch["sampling_rate"] = resampler.new_freq
+#     batch["sentence"] = re.sub(chars_to_ignore_regex, '', batch["sentence"]).lower().replace("’", "'")
+#     return batch
+
 def map_to_array(batch):
-    speech, _ = torchaudio.load(batch["path"])
-    batch["speech"] = resampler.forward(speech.squeeze(0)).numpy()
-    batch["sampling_rate"] = resampler.new_freq
-    batch["sentence"] = re.sub(chars_to_ignore_regex, '', batch["sentence"]).lower().replace("’", "'")
+    speech, _ = torchaudio.load(batch["file"])
+    batch["speech"] = speech
     return batch
 
-
 def map_to_pred(batch):
-    features = processor(batch["speech"], sampling_rate=batch["sampling_rate"][0], padding=True, return_tensors="pt")
+    # features = processor(batch["speech"][0], sampling_rate=batch["sampling_rate"][0], padding=True, return_tensors="pt")
+    features = processor(batch["speech"], sampling_rate=16_000, padding=True, return_tensors="pt")
+
     input_values = features.input_values.to(device)
+
     attention_mask = features.attention_mask.to(device)
     with torch.no_grad():
         logits = model(input_values, attention_mask=attention_mask).logits
+
     pred_ids = torch.argmax(logits, dim=-1)
     batch["predicted"] = processor.batch_decode(pred_ids)
-    batch["target"] = batch["sentence"]
+    # batch["target"] = batch["sentence"]
+    batch["target"] = batch["text"]
+
+
+    # add lm
     return batch
 
 def show_random_elements(dataset, out, num_examples=10):
@@ -47,30 +61,30 @@ def show_random_elements(dataset, out, num_examples=10):
         print(df, file=ex_log)
 
 def main():
-    ds = load_dataset("common_voice", "fr", split="test[:40%]", data_dir="./cv-corpus-6.1-2020-12-11")
+    # ds = load_dataset("common_voice", "fr", split="test[:40%]", data_dir="./cv-corpus-6.1-2020-12-11")
+    ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
+
     ds = ds.map(map_to_array)
     start = time.time()
     result = ds.map(map_to_pred, batched=True, batch_size=16, remove_columns=list(ds.features.keys()))
-    wer = load_metric("wer")
     end  = time.time()
     duration = end - start
-    print(result)
+    # print(result)
     # results = result["train"]
     results = result
-    wer_metric = load_metric("wer")
-
+    wer_metric = load_metric("wer", num_process=4)
+    wer = wer_metric.compute(predictions=results["predicted"], references=results["target"])
     print(f"Dataset : Common-voice FR train split" + 
         f"\nInference time : {duration:.2f} \n" + 
-        "Test WER: {:.3f}".format(wer_metric.compute(predictions=results["predicted"], references=results["target"])))
+        "Test WER: {:.3f}".format(wer))
     print("\n")
     show_random_elements(results, out, num_examples=10)
     wer_log = os.path.join(out, "wer_cv.txt")
     with open(wer_log, "w") as err_file:
         print(f"Dataset : Common-voice FR train split" + 
         f"\nInference time : {duration:.2f} \n" + 
-        "Test WER: {:.3f}".format(wer_metric.compute(predictions=results["predicted"], references=results["target"])), file=err_file)
+        "Test WER: {:.3f}".format(wer), file=err_file)
 
-    print(wer.compute(predictions=result["predicted"], references=result["target"]))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--out", default=None, type=str,
@@ -88,9 +102,9 @@ out = args.out
 device = "cuda"
 chars_to_ignore_regex = '[\,\?\.\!\-\;\:\"]'  # noqa: W605
 resampler = torchaudio.transforms.Resample(orig_freq=48_000, new_freq=16_000)
-# processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53-french")
-# model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53-french").to(device)
-processor = Wav2Vec2Processor.from_pretrained(processor_dir)
-model = Wav2Vec2ForCTC.from_pretrained(model_dir).to("cuda")
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53-french")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53-french").to(device)
+# processor = Wav2Vec2Processor.from_pretrained(processor_dir)
+# model = Wav2Vec2ForCTC.from_pretrained(model_dir).to("cuda")
 
 main()
